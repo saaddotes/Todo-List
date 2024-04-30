aba5abca4d7f1c5ec697cc990092b2dd059ae667
cadetsaad <cadetsaadsoomro@gmail.com>
2024-04-30 15:31:59 +0500

    Modified Logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let list = document.getElementById('todo-list');
-const todoData = [];
+let todoData = [];
 
 // if (todoData.length == 0) {
 //     list.innerHTML = "<div><i class='fa-regular fa-circle-check'></i> You don't have any task yet</div>"
@@ -7,27 +7,37 @@ const todoData = [];
 
 
 function actionToAdd(id = 'task_input') {
+    // Getting value from input field
     let taskInput = document.getElementById(id);
     let taskValue = taskInput.value.trim();
+
+    // Checking if it is not empty
     if (taskValue !== '') {
-        todoData.push(taskValue);
+        // Storing Data as object
+        todoData.push({ task: taskValue, checked: false });
         list.innerHTML = "";
         todoData.forEach(addData);
         taskInput.value = "";
     }
 
+};
 
-}
 
-
-var tabs = document.querySelectorAll('.tab');
+let tabs = document.querySelectorAll('.tab');
 tabs.forEach(tab => {
     tab.addEventListener('click', tabSwitch);
     console.log(tab.innerHTML);
 });
 
+
 function tabSwitch() {
-    var tabs = document.querySelectorAll('.tab');
+    let tabs = document.querySelectorAll('.tab');
+
+    let active = document.querySelector('.active')
+    if (active.innerHTML === 'All') {
+        updateData();
+    }
+    console.log()
     tabs.forEach(tab => {
         tab.classList.remove('active');
 
@@ -35,15 +45,30 @@ function tabSwitch() {
 
     this.classList.add('active');
     if (this.innerHTML == 'Completed') {
-        list.innerHTML = '<div class="text-center"><i class="fa-solid fa-person-digging"></i>  Still  working on this Section </div>'
+        list.innerHTML = "";
+        todoData.forEach((task) => {
+            if (task.checked) {
+                console.log(task.checked)
+                addData(task)
+            }
+        });
+        // list.innerHTML = '<div class="text-center"><i class="fa-solid fa-person-digging"></i>  Still  working on this Section </div>'
     }
 
     else if (this.innerHTML == 'Pending') {
-        list.innerHTML = '<div class="text-center"><i class="fa-solid fa-person-digging"></i>  Still  working on this Section </div>'
+        list.innerHTML = "";
+        todoData.forEach((task) => {
+            if (!task.checked) {
+                console.log(task.checked)
+                addData(task)
+            }
+        });
+        // list.innerHTML = '<div class="text-center"><i class="fa-solid fa-person-digging"></i>  Still  working on this Section </div>'
 
     }
 
     else {
+        // updateData()
 
         if (todoData.length == 0) {
             list.innerHTML = "<div><i class='fa-regular fa-circle-check'></i> You don't have any task yet</div>"
@@ -54,36 +79,63 @@ function tabSwitch() {
             todoData.forEach(addData);
         }
     }
-}
+};
 
 
-function addData(value) {
-    list.innerHTML += `<li class="border rounded d-flex align-items-center justify-content-between p-1 mt-1">
-                            <span class="d-flex align-items-center gap-2">
-                                <input class="largerCheckBox" type="checkbox"><span>${value}</span>
+function addData(data) {
+    list.innerHTML += `<li class="border rounded d-flex align-items-center justify-content-between p-1 mt-1 item">
+                            <span class="d-flex align-items-center gap-2 ">
+                                <input class="largerCheckBox " type="checkbox" ${data.checked ? 'checked' : ''}>
+                                <span id='value'>${data.task}</span>
                             </span>
                             <span class="d-flex gap-3">
-                                <i class="fa-solid fa-trash fs-5" role="button" onclick="removeTask('${value}')"></i>
-                                <i class="text-primary fa-solid fa-pen fs-5" role="button" onclick="editTask('${value}')"></i>
+                                <i class="fa-solid fa-trash fs-5" role="button" onclick="removeTask('${data.task}')"></i>
+                                <i class="text-primary fa-solid fa-pen fs-5" role="button" onclick="editTask('${data.task}')"></i>
                             </span>
                         </li>`;
+};
 
 
-}
+function removeTask(task) {
+    const indexToRemove = todoData.findIndex(item => item.task === task);
+    console.log('removing', task, indexToRemove)
 
-function removeTask(value) {
-    const indexToRemove = todoData.indexOf(value);
     if (indexToRemove !== -1) {
         todoData.splice(indexToRemove, 1);
         list.innerHTML = "";
         todoData.forEach(addData);
     }
-}
+};
+
+
+function updateData() {
+    console.log("Working");
+    todoData = []
+    let allItems = document.querySelectorAll('.item');
+    for (let i of allItems) {
+        let task = i.querySelector('#value')
+        let taskCheck = i.querySelector('input');
+        todoData.push
+        todoData.push({ task: task.innerHTML, checked: taskCheck.checked });
+    }
+
+
+    // let allItems = document.querySelectorAll('.item');
+    // list.innerHTML = '';
+    // for (let i of allItems) {
+    //     let inputElement = i.querySelector('input');
+    //     let val = i.querySelector('#value')
+    //     if(inputElement.checked) {
+    //         list.innerHTML += i.innerHTML
+    //     }
+    // }
+};
+
 
 function editTask(value) {
     let editBox = document.querySelector('.editBox')
     editBox.style.display = 'block';
-    const indexToEdit = todoData.indexOf(value);
+    const indexToEdit = todoData.findIndex(item => item.task === value);
     if (indexToEdit !== -1) {
         console.log("Working")
         todoData.splice(indexToEdit, 1);
@@ -92,17 +144,17 @@ function editTask(value) {
         console.log(taskInput)
         taskInput.value = value
     }
-}
+};
+
 
 function editSave() {
-
     // todoData.splice(indexToEdit, 1);
     let editBox = document.querySelector('.editBox')
     editBox.style.display = 'none';
     let editInput = document.querySelector('.editInput');
     let taskValue = editInput.value.trim();
     if (taskValue !== '') {
-        todoData.push(taskValue);
+        todoData.push({task: taskValue , checked: false});
         list.innerHTML = "";
         todoData.forEach(addData);
         editInput.value = "";
@@ -111,9 +163,10 @@ function editSave() {
     console.log('EDIT Save')
     // let taskInput = document.getElementById('task_input');
     // taskInput.value = value;
-}
+};
+
 
 function clearAllData() {
     todoData.length = 0
     list.innerHTML = "";
-}
+};
